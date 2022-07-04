20569941879b87f79b79faefc45ff43511e917e9
julijtt1 <juliatudurit@gmail.com>
2022-07-04 14:12:34 +0200

    461

--- src/containers/LocationListContainer.js
+++ src/containers/LocationListContainer.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { setSelectedCity, setWeather } from "../actions";
+import * as actions from "../actions";
 import LocationList from "../components/LocationList";
-import { getWeatherCities } from "../reducers";
+import { getCity, getWeatherCities } from "../reducers";
 import PropTypes from "prop-types";
 
 class LocationListContainer extends Component {
@@ -26,17 +27,17 @@ class LocationListContainer extends Component {
 
 LocationListContainer.propTypes = {
   setSelectedCity: PropTypes.func.isRequired,
+  setWeather: PropTypes.func.isRequired,
   cities: PropTypes.array.isRequired,
   citiesWeather: PropTypes.array.isRequired,
+  citiy: PropTypes.string.isRequired,
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  setSelectedCity: (value) => dispatch(setSelectedCity(value)),
-  setWeather: (cities) => dispatch(setWeather(cities)),
-});
+const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch);
 
 const mapStateToProps = (state) => ({
   citiesWeather: getWeatherCities(state),
+  city: getCity(state),
 });
 
 export default connect(
